2680f254b7de563211a22b061ad4c3bbfa816724
2akhairo <moudroff@gmail.com>
2023-03-17 18:50:51 +0300

    EF

---
+++ MVCwithEF/MVCwithEF/Data/ProductsContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVCwithEF.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCwithEF.Data
+{
+    public class ProductsContext:DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public ProductsContext(DbContextOptions<ProductsContext> options)
+          : base(options)
+        {
+            Database.EnsureCreated();   // создаем базу данных при первом обращении
+        }
+
+
+    }
+}
